3793d9e8290147f80650ef1e79ff70422a4c3df7
Cullen Daugherty <107732615+DakJaniels@users.noreply.github.com>
2024-10-21 17:41:15 -0500

    localize "time" to scope of function.

--- CCTracker.lua
+++ CCTracker.lua
@@ -145,7 +145,7 @@ end
 
 function CCTracker:HandleEffectsChanged(_,changeType,_,eName,unitTag,beginTime,endTime,_,_,_,buffType,abilityType,_,unitName,_,aId,_)
 	--  if self.SV.debug.enabled then self.debug:Print(unitName.." - "..GetUnitName("player")) end
-	time = GetFrameTimeMilliseconds()
+	local time = GetFrameTimeMilliseconds()
 	if not (unitTag == "player" or unitName == self.currentCharacterName) then
 		return
 	else
